26e1b94abd944812838406689548790cceade2d9
duckietm <troop@mailtjes.com>
2025-03-12 14:21:38 +0100

    :up: small fix loading screen & orde css files

--- src/components/loading/LoadingView.tsx
+++ src/components/loading/LoadingView.tsx
@@ -1,33 +1,29 @@
 import { FC } from 'react';
 import { Base, Column, Text } from '../../common';
 
-interface LoadingViewProps
-{
+interface LoadingViewProps {
     isError: boolean;
     message: string;
 }
 
-export const LoadingView: FC<LoadingViewProps> = props =>
-{
+export const LoadingView: FC<LoadingViewProps> = props => {
     const { isError = false, message = '' } = props;
     
     return (
         <Column fullHeight position="relative" className="nitro-loading">
             <Base fullHeight className="container h-100">
-                <Column fullHeight alignItems="center" justifyContent="end">
+                <Column fullHeight alignItems="center" justifyContent="center">
                     <Base className="nitro-loading_animation" />
-					<Base className="nitro-loading_logo" /> 
-                    <Column size={ 6 } className="text-center py-4">
-                        { isError && (message && message.length) ?
-                            <Base className="fs-4 text-shadow">{ message }</Base>
-                            :
-                            <>
-                                <Text fontSizeCustom={ 36 } variant="white" className="text-shadow">The hotel is loading ...</Text>
-                            </>
-                        }
-                    </Column>
+                    <Base className="nitro-loading_logo" />
+                    { isError && (message && message.length) ?
+                        <Base className="fs-4 nitro-loading_text">{ message }</Base>
+                        :
+                        <Text fontSizeCustom={32} variant="white" className="nitro-loading_text">
+                            The hotel is loading ...
+                        </Text>
+                    }
                 </Column>
             </Base>
         </Column>
     );
-}
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import './css/chats.css';
+
 import './css/index.css';
-import './css/loading.css';
 import './css/NitroCardView.css';
-import './css/NotificationCenterView.css';
 import './css/PurseView.css';
+
+import './css/chat/chats.css';
+
+import './css/loading/loading.css';
+
+import './css/notification/NotificationCenterView.css';
 
 import './css/room/ChatHistoryView.css';
 import './css/room/RoomWidgets.css';
